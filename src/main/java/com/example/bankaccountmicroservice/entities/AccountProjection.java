9daec2632937bc53630ffecd3fde73db67a2c7e2
Mohamed-Elalaoui <mohamed.du96@gmail.com>
2022-10-28 05:21:54 +0100

    SpringDataRest

---
+++ src/main/java/com/example/bankaccountmicroservice/entities/AccountProjection.java
@@ -0,0 +1,13 @@
+package com.example.bankaccountmicroservice.entities;
+
+import com.example.bankaccountmicroservice.enums.AccountType;
+import org.springframework.data.rest.core.config.Projection;
+
+@Projection(types = BankAccount.class,name = "p1")
+public interface AccountProjection {
+    public String getId();
+
+    public AccountType getType();
+
+    public Double getBalance();
+}
